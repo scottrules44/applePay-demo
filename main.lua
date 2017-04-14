03038882b3f675b52f0897b94ec9300856af41e5
Scott Harrison <scottrules44@gmail.com>
2017-04-14 00:01:15 -0500

    1.0
    1.0

---
+++ main.lua
@@ -0,0 +1,21 @@
+local applePay = require("plugin.applePay")
+local status = display.newText("Apple Pay", display.contentCenterX, display.contentCenterY, system.nativeFont, 20)
+local didComplete = false
+applePay.init("merchant.com.test1234", "pk_test_zSlwAfUSUiZWbCHXthmJlCyJ", function (e)
+    if e.status == "showing" then
+        status.text = "showing"
+    end
+    if e.status == "completed" then
+        didComplete = true
+        status.text = "completed"
+    end
+    if e.status == "hiding" then
+        if didComplete == false then
+            status.text = "failed"
+        end
+        didComplete = false
+    end
+
+end)
+applePay.pay({{label="Scott", amount = 200.00}, {label="Rob", amount = 300.00}, {label="Brent", amount = .01}}, "USD")
+
