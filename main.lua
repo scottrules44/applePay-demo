d70f4b77d9467901ac95ede600bb0ec197db364c
scottrules44 <scottrules44@gmail.com>
2017-04-24 23:15:14 -0500

    1.2
    1.2

--- main.lua
+++ main.lua
@@ -1,13 +1,20 @@
 local applePay = require("plugin.applePay")
+local json = require("json")
 local status = display.newText("Apple Pay", display.contentCenterX, display.contentCenterY, system.nativeFont, 20)
 local didComplete = false
-applePay.init("merchantID", "stripeKey", function (e)
+local secretKey = "secretKey"
+applePay.init("merchant.com.test1234", "pk_test_zSlwAfUSUiZWbCHXthmJlCyJ", function (e)
     if e.status == "showing" then
         status.text = "showing"
     end
     if e.status == "completed" then
         didComplete = true
         status.text = "completed"
+        network.request( "https://api.stripe.com/v1/charges", "POST", function ( ev )
+            if ev.isError then
+                status.text = "Payment could not be processed"
+            end
+        end, {headers= {["Content-Type"]= "application/x-www-form-urlencoded", ["Authorization"]= "Bearer "..secretKey}, body= "amount=50001".."&currency=usd".."&source="..e.token.."&description=Pay Rob, Scott, and Brent" } ) -- 50001 = 500.01
     end
     if e.status == "hiding" then
         if didComplete == false then
@@ -17,5 +24,5 @@ applePay.init("merchantID", "stripeKey", function (e)
     end
 
 end)
-applePay.pay({{label="Scott", amount = 200.00}, {label="Rob", amount = 300.00}, {label="Brent", amount = .01}}, "USD")
+applePay.requestToken({{label="Scott", amount = 200.00}, {label="Rob", amount = 300.00}, {label="Brent", amount = .01}}, "USD")
 
